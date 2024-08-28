28feebacdf869cde259731fc0ce3f809e918cb9c
yassinejellouli <testyeees@gmail.com>
2024-08-28 21:51:16 +0100

    hotfix v8

--- src/Head.js
+++ src/Head.js
@@ -6,9 +6,9 @@ function Header() {
   return (
    
       <div className="headerr" style={{ padding: "10px" }}>
-        <Link to="/" style={{ textDecoration: 'none'}}><span className="glowing-text" >HOME</span></Link>
-        <Link to="/Experience" style={{ textDecoration: 'none'}}><span className="glowing-text">EXPERIENCE</span></Link>
-        <Link to="/Project" style={{ textDecoration: 'none'}}> <span className="glowing-text" style={{ "margin-right": "20px" }}>
+        <Link to="/" style={{ textDecoration: 'none'}}><span  className="glowing-text " >HOME</span></Link>
+        <Link to="/Experience" style={{ textDecoration: 'none'}}><span className="glowing-text forshrink">EXPERIENCE</span></Link>
+        <Link to="/Project" style={{ textDecoration: 'none'}}> <span className="glowing-text forshrink" style={{ "margin-right": "20px" }}>
           PROJECTS
         </span></Link>
        
